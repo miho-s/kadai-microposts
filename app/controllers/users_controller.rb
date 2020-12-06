59db0e201c130d8ea0def58250633f6794be4c93
miho.s <s.mihoxx@gmail.com>
2020-12-06 02:57:49 +0000

    1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :likes, :myfavorite]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :likes]
   
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
@@ -45,11 +45,7 @@ class UsersController < ApplicationController
     counts(@user)
   end
   
-  def myfavorite
-    @user = User.find(params[:id])
-    @favorites = @user.fav_ing.order(id: :desc).page(params[:page])
-    counts(@user)
-  end
+
   
 private
 
